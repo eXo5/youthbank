b5e4f8f51e22b541b9e598022eb713a780b83895
jessitea <JessiPro@Jessicas-MacBook-Pro.local>
2017-07-27 19:55:24 -0400

    Parent View, new componenets made

---
+++ src/components/ChildCards.js
@@ -0,0 +1,35 @@
+import {Collapsible, CollapsibleItem, Col} from 'react-materialize';
+import React from 'react';
+import List from './List';
+import './viewParent.css';
+
+class ChildCards extends React.Component {
+
+render(){
+
+  return(
+      <Col s={12}>
+
+        <div>
+
+          <Collapsible popout>
+            <CollapsibleItem className='childCard' header='Jane' icon='filter_drama'>
+              <List />
+            </CollapsibleItem>
+          </Collapsible>
+
+        <Collapsible popout>
+          <CollapsibleItem className='childCard' header='Sarah' icon='filter_drama'>
+            <List />
+          </CollapsibleItem>
+        </Collapsible>
+        
+      </div>
+
+    </Col>
+
+      )
+  }
+}
+
+export default ChildCards;
--- src/components/ViewParent.js
+++ src/components/ViewParent.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import {Collapsible, CollapsibleItem} from 'react-materialize';
+import {SideNav, SideNavItem, Button, Col, Footer, Dropdown, NavItem, Modal, Row, Icon, Input} from 'react-materialize';
 import './viewParent.css';
-import List from './List';
+// import List from './List';
+import ChildCards from './ChildCards';
+import banner from '../img/banner-parent.png';
+import navBg from '../img/nav-background.jpg';
+import icon from '../img/vectorParent.png';
 
 
 class ViewParent extends React.Component {
@@ -9,36 +13,84 @@ class ViewParent extends React.Component {
 render() {
 
   return(
-  <div className="container">
+    <div className="container">
 
-      <div className="row">
-          <Collapsible popout>
-            <CollapsibleItem className='childCard' header='Jane' icon='filter_drama'>
-              <List />
-            </CollapsibleItem>
-          </Collapsible>
+      {/* START OF SIDE NAVBAR */}
+      <SideNav
+        trigger={<Button className="menuBtn">MENU</Button>}
+        options={{ closeOnClick: false }}
+      >
+        <SideNavItem userView
+          user={{
+            background: navBg,
+            image: icon,
+            name: 'John Doe'
+          }}
+        />
 
-        <Collapsible popout>
-          <CollapsibleItem className='childCard' header='Sarah' icon='filter_drama'>
-            <List />
-          </CollapsibleItem>
-        </Collapsible>
+      {/* MANAGE TASKS */}
+        <Dropdown trigger={
+          <SideNavItem href='#!icon' icon='list'>Manage Tasks</SideNavItem>
+          }>
+
+          {/* MODAL FOR NEW TASKS */}
+          <Modal
+            header='Add A New Task'
+            trigger={
+              <NavItem>Add A New Task</NavItem>
+            }>
+            <Row>
+              <Input s={12} label="Task"><Icon>build</Icon></Input>
+              <Input s={12} label="Amount"><Icon>attach money</Icon></Input>
+              <Button waves='light' node='a' className="mainBtn" href='http://www.google.com'>Submit</Button>
+            </Row>
+          </Modal>
+          
+          <NavItem>Edit An Existing Task</NavItem>
+        </Dropdown>
+        
+         {/* MANAGE CHILDREN */}
+        <Dropdown trigger={
+            <SideNavItem href='#!second' icon='face'>Manage Children</SideNavItem>
+          }>
+
+        {/* MODAL FOR NEW CHILD */}
+          <Modal
+            header='Set Up a New Account for Child'
+            trigger={
+              <NavItem>Add A New Child</NavItem>
+            }>
+            <Row>
+              <Input s={12} label="Child Name"><Icon>face</Icon></Input>
+
+              <Input s={12} label="User Name"></Input>
+              <Input s={12} label="Password"></Input>
+              <Button waves='light' node='a' className="mainBtn" href='http://www.google.com'>Submit</Button>
+            </Row>
+          </Modal>
+          
+          <NavItem>Edit An Exisiting Child</NavItem>
+        </Dropdown>
+
+         {/* MENU FOOTER */}
+        <Footer className="page-footer">
+            <Button waves='light' className="mainBtn">LogOut</Button>
+        </Footer>
+
+      </SideNav>
+    {/* END OF SIDE NAVBAR */}
+
+      {/* BANNER FOR PARENT VIEW */}
+      <div className="row bannerDiv">
+        <Col s={12}>
+        <img src={banner} className="bannerParent" />
+        </Col>
       </div>
 
+      {/* CHILD DISPLAY */}
       <div className="row">
-        <Collapsible popout>
-          <CollapsibleItem className='childCard' header='Mike' icon='filter_drama'>
-            <List />
-          </CollapsibleItem>
-        </Collapsible>
-
-        <Collapsible popout>
-          <CollapsibleItem className='childCard' header='George' icon='filter_drama'>
-            <List />
-          </CollapsibleItem>
-         </Collapsible>
-       </div>
-
+        <ChildCards />
+      </div>
     </div>
   )
 }
