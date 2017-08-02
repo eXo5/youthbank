e776bec02c545e9217dfd2793875a3e44dd4543b
jdalton467 <jamesdalton463@gmail.com>
2017-08-02 10:54:15 -0400

    push

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
 import React, {Component} from 'react';
 
 import {Row, Col, Form, Button, Carousel, Modal, Footer, Input, Card, CardTitle} from 'react-materialize';
-import Navbar from './components/WelcomeView/Navbar'
+import PgNavbar from './components/WelcomeView/Navbar'
 import Home from './components/WelcomeView/SignUp';
 import './index.css';
 import logo from './logo.svg';
@@ -11,21 +10,22 @@ import Why from './components/WelcomeView/WhyWeMadeIt';
 import Features1 from './components/WelcomeView/Features1';
 
 
+
 const DisplayLinks = props => {
-	if(props.loggedIn) {
-		return(
-			<h2>Logged In</h2>
-			)
-	}else{
-		return(
-			<h2>Not Logged In</h2>
-			)
-	}
+  if(props.loggedIn) {
+    return(
+      <h2>Logged In</h2>
+      )
+  }else{
+    return(
+      <h2>Not Logged In</h2>
+      )
+  }
 }
 
 class App extends React.Component {
 
-	constructor() {
+  constructor() {
     super()
 
     this.state = {
@@ -38,146 +38,81 @@ class App extends React.Component {
   }
 
   componentDidMount(){
-  	axios.get("/auth/user/").then(response => {
-  		console.log(response.data)
-  		if(!!response.data.user) {
-  			console.log("USER PRESENT")
-  			this.setState({
-  				loggedIn: true,
-  				user: response.data.user
-  			})
-  		}else{
-  			this.setState({
-  				loggedIn: false,
-  				user: null
-  			})
-  		}
-  	})
+    axios.get("/auth/user/").then(response => {
+      console.log(response.data)
+      if(!!response.data.user) {
+        console.log("USER PRESENT")
+        this.setState({
+          loggedIn: true,
+          user: response.data.user
+        })
+      }else{
+        this.setState({
+          loggedIn: false,
+          user: null
+        })
+      }
+    })
   }
 
   _loginParent = (email, password) => {
-  	axios
-  		.post("/auth/login/parent", {
-  			email,
-  			password
-  		})
-  		.then(response => {
-  			console.log(response)
-  			if(response.status === 200) {
-  				this.setState({
-  					loggedIn: true,
-  					user: response.data.user
-  				})
-  			}
-  		})
+    axios
+      .post("/auth/login/parent", {
+        email,
+        password
+      })
+      .then(response => {
+        console.log(response)
+        if(response.status === 200) {
+          this.setState({
+            loggedIn: true,
+            user: response.data.user
+          })
+        }
+      })
   }
 
 
   _logout = (event) => {
-  	event.preventDefault()
-  	console.log("Logged Out")
-  	axios.post("/auth/logout")
-  		.then(response => {
-  			if (response.status === 200) {
-  				this.setState({
-  					loggedIn: false,
-  					user: null
-  				})
-  			}
-  		})
+    event.preventDefault()
+    console.log("Logged Out")
+    axios.post("/auth/logout")
+      .then(response => {
+        if (response.status === 200) {
+          this.setState({
+            loggedIn: false,
+            user: null
+          })
+        }
+      })
   }
 
 
   render() {
-  	// if (this.state.redirectTo){
-  	// 	return <Redirect to={{pathname: this.state.redirectTo}} />
-  	// }
-
-
-  _logout = (event) => {
-  	event.preventDefault()
-  	console.log("Logged Out")
-  	axios.post("/auth/logout")
-  		.then(response => {
-  			if (response.status === 200) {
-  				this.setState({
-  					loggedIn: false,
-  					user: null
-  				})
-  			}
-  		})
-  }
-
-
-  render() {
-  	// if (this.state.redirectTo){
-  	// 	return <Redirect to={{pathname: this.state.redirectTo}} />
-  	// }
-
-    return (
-    	<Row>
-    	<header>
-    		<Navbar brand='KidsBank' right>
-    	{/*we have to import react-router */}
-					<NavItem href='get-started.html'>Getting started</NavItem>
-					<NavItem href='components.html'>Components</NavItem>
-					<NavItem>
-						<Link to="/signin">
-						<Button waves="light">Sign In</Button>
-						</Link>
-					</NavItem>
-				</Navbar>
-		</header>
-		<Row></Row>
-		<main>
-
-		<Switch>
-			<Route exact path="/" render={() => <Home saveUser={this.saveUser}/>} />
-			<Route exact path="/signin" render={() => <SignIn _login={this._loginParent}/>} />
-		</Switch>
-
-		</main>
-    	{/* FOOTER */}
-	    	<Footer copyrights="&copy 2015 Copyright Text"
-				moreLinks={
-					<a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-				}
-				links={
-					<ul>
-						<li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-						<li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-						<li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-						<li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-					</ul>
-				}
-				className='example page-footer'>
-					<h5 className="white-text"> Kids Bank</h5>
-					<p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-			</Footer>
-	</Row>
-
+    // if (this.state.redirectTo){
+    //  return <Redirect to={{pathname: this.state.redirectTo}} />
+    // }
 
     return (
       <div>
-		<Switch>
-			<Route exact path="/" render={() => <SignUp saveUser={this.saveUser}/>} />
-			<Route exact path="/signin" render={() => <SignIn _login={this._loginParent}/>} />
-		</Switch>
+    <Switch>
+      <Route exact path="/" render={() => <SignUp saveUser={this.saveUser}/>} />
+      <Route exact path="/signin" render={() => <SignIn _login={this._loginParent}/>} />
+    </Switch>
 
 
 
     <div> 
-	    	<header>
-	    		<Navbar />
-			</header>
-			  <Home />
-			 <Why />
-			  <Features1 />
-			{/*<PgFooter /> */}
+        <header>
+          <Navbar />
+      </header>
+        <Home />
+       <Why />
+        <Features1 />
+      {/*<PgFooter /> */}
 
-	</div>
+  </div>
 </div>
-
     );
   }
 }
