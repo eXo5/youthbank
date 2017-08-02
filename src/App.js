6454cf875ea5ade8954bf9441230a996bed64190
jdalton467 <jamesdalton463@gmail.com>
2017-08-02 10:56:48 -0400

    push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+
 import React, {Component} from 'react';
 
 import {Row, Col, Form, Button, Carousel, Modal, Footer, Input, Card, CardTitle} from 'react-materialize';
-import PgNavbar from './components/WelcomeView/Navbar'
+import Navbar from './components/WelcomeView/Navbar'
 import Home from './components/WelcomeView/SignUp';
 import './index.css';
 import logo from './logo.svg';
@@ -10,22 +11,21 @@ import Why from './components/WelcomeView/WhyWeMadeIt';
 import Features1 from './components/WelcomeView/Features1';
 
 
-
 const DisplayLinks = props => {
-  if(props.loggedIn) {
-    return(
-      <h2>Logged In</h2>
-      )
-  }else{
-    return(
-      <h2>Not Logged In</h2>
-      )
-  }
+	if(props.loggedIn) {
+		return(
+			<h2>Logged In</h2>
+			)
+	}else{
+		return(
+			<h2>Not Logged In</h2>
+			)
+	}
 }
 
 class App extends React.Component {
 
-  constructor() {
+	constructor() {
     super()
 
     this.state = {
@@ -38,80 +38,84 @@ class App extends React.Component {
   }
 
   componentDidMount(){
-    axios.get("/auth/user/").then(response => {
-      console.log(response.data)
-      if(!!response.data.user) {
-        console.log("USER PRESENT")
-        this.setState({
-          loggedIn: true,
-          user: response.data.user
-        })
-      }else{
-        this.setState({
-          loggedIn: false,
-          user: null
-        })
-      }
-    })
+  	axios.get("/auth/user/").then(response => {
+  		console.log(response.data)
+  		if(!!response.data.user) {
+  			console.log("USER PRESENT")
+  			this.setState({
+  				loggedIn: true,
+  				user: response.data.user
+  			})
+  		}else{
+  			this.setState({
+  				loggedIn: false,
+  				user: null
+  			})
+  		}
+  	})
   }
 
   _loginParent = (email, password) => {
-    axios
-      .post("/auth/login/parent", {
-        email,
-        password
-      })
-      .then(response => {
-        console.log(response)
-        if(response.status === 200) {
-          this.setState({
-            loggedIn: true,
-            user: response.data.user
-          })
-        }
-      })
+  	axios
+  		.post("/auth/login/parent", {
+  			email,
+  			password
+  		})
+  		.then(response => {
+  			console.log(response)
+  			if(response.status === 200) {
+  				this.setState({
+  					loggedIn: true,
+  					user: response.data.user
+  				})
+  			}
+  		})
   }
 
 
   _logout = (event) => {
-    event.preventDefault()
-    console.log("Logged Out")
-    axios.post("/auth/logout")
-      .then(response => {
-        if (response.status === 200) {
-          this.setState({
-            loggedIn: false,
-            user: null
-          })
-        }
-      })
+  	event.preventDefault()
+  	console.log("Logged Out")
+  	axios.post("/auth/logout")
+  		.then(response => {
+  			if (response.status === 200) {
+  				this.setState({
+  					loggedIn: false,
+  					user: null
+  				})
+  			}
+  		})
   }
 
 
+
   render() {
-    // if (this.state.redirectTo){
-    //  return <Redirect to={{pathname: this.state.redirectTo}} />
-    // }
+  	// if (this.state.redirectTo){
+  	// 	return <Redirect to={{pathname: this.state.redirectTo}} />
+  	// }
+
+
+
 
     return (
       <div>
-    <Switch>
-      <Route exact path="/" render={() => <SignUp saveUser={this.saveUser}/>} />
-      <Route exact path="/signin" render={() => <SignIn _login={this._loginParent}/>} />
-    </Switch>
+		<Switch>
+			<Route exact path="/" render={() => <SignUp saveUser={this.saveUser}/>} />
+			<Route exact path="/signin" render={() => <SignIn _login={this._loginParent}/>} />
+		</Switch>
 
 
 
     <div> 
-        <header>
-          <Navbar />
-      </header>
-        <Home />
-       <Why />
-        <Features1 />
-      {/*<PgFooter /> */}
+	    	<header>
+	    		<Navbar />
+			</header>
+			  <Home />
+			 <Why />
+			  <Features1 />
+			{/*<PgFooter /> */}
 
-  </div>
+	</div>
 </div>
     );
   }
