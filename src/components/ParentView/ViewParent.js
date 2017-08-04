86c4cf72b05fdbd67c693d448e870832871c494d
Oscar Abella <oscarabella@mothership.local>
2017-08-04 14:38:36 -0400

    navbar parent/child/welcome & content

--- src/components/ParentView/ViewParent.js
+++ src/components/ParentView/ViewParent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SideNav, SideNavItem, Button, Col, Footer, Dropdown, NavItem, Modal, Row, Icon, Input, Card, CardTitle} from 'react-materialize';
+import {SideNav, SideNavItem, Button, Col, Footer, Dropdown, Navbar, NavItem, Modal, Row, Icon, Input, Card, CardTitle} from 'react-materialize';
 import '../../index.css';
 // import List from './List';
 import ChildCards from './ChildCards';
@@ -71,88 +71,99 @@ class ViewParent extends React.Component {
 
     }//end of handleNewChild
 
-
-
   render() {
 
     return(
       <div>
 
         {/* START OF SIDE NAVBAR */}
-        <SideNav
-          trigger={<Button className="menuBtn right">MENU</Button>}
-          options={{ closeOnClick: false }}
-        >
-          <SideNavItem userView
-            user={{
-              background: navBg,
-              image: icon,
-              name: 'John Doe'
-            }}
-          />
+      <header>
+          <div className="">
+              <Navbar brand='Young Money' className="NavbarCSS">
+            {/*we have to import react-router */}
 
-        {/* MANAGE TASKS */}
-          <Dropdown trigger={
-            <SideNavItem href='#!icon' icon='list'>Manage Tasks</SideNavItem>
-            }>
+                <li className="navItems">
+                <SideNav
+                          trigger={<Button className="menuBtn">MENU</Button>}
+                          options={{ closeOnClick: false }}
+                        >
+                          <SideNavItem userView
+                            user={{
+                              background: navBg,
+                              image: icon,
+                              name: 'John Doe'
+                            }}
+                          />
 
-            {/* MODAL FOR NEW TASKS */}
-            <Modal
-              header='Add A New Task'
-              fixedFooter
-              trigger={
-                <NavItem>Add A New Task</NavItem>
-              }>
-              <Row>
-                <form>
-                  <Input s={12} label="Task" id="task" value={this.state.task} onChange={this.handleChange}><Icon>build</Icon></Input>
-                  <Input s={12} label="Description of Task" id="descript" value={this.state.descript} onChange={this.handleChange}><Icon></Icon></Input>
-                  <Input s={12} label="Amount" id="amount" value={this.state.amount} onChange={this.handleChange}><Icon></Icon></Input>
+                        {/* MANAGE TASKS */}
+                          <Dropdown trigger={
+                            <SideNavItem href='#!icon' icon='list'>Manage Tasks</SideNavItem>
+                            }>
+
+                            {/* MODAL FOR NEW TASKS */}
+                            <Modal
+                              header='Add A New Task'
+                              fixedFooter
+                              trigger={
+                                <NavItem>Add A New Task</NavItem>
+                              }>
+                              <Row>
+                                <form>
+                                  <Input s={12} label="Task" id="task" value={this.state.task} onChange={this.handleChange}><Icon>build</Icon></Input>
+                                  <Input s={12} label="Description of Task" id="descript" value={this.state.descript} onChange={this.handleChange}><Icon></Icon></Input>
+                                  <Input s={12} label="Amount" id="amount" value={this.state.amount} onChange={this.handleChange}><Icon></Icon></Input>
+                                  
+                                  <Button type="submit" waves='light' className="mainBtn">Submit</Button>
+                                </form>
+                              </Row>
+                            </Modal>
+                            
+                            <NavItem>Edit An Existing Task</NavItem>
+                          </Dropdown>
+                          
+                           {/* MANAGE CHILDREN */}
+                          <Dropdown trigger={
+                              <SideNavItem href='#!second' icon='face'>Manage Children</SideNavItem>
+                            }>
+
+                          {/* MODAL FOR NEW CHILD */}
+                            <Modal
+                              header='Set Up a New Account for Child'
+                              fixedFooter
+                              trigger={
+                                <NavItem>Add A New Child</NavItem>
+                              }>
+                              <Row>
+                                <form>
+                                  <Input s={6} label="First Name" id="firstName" value={this.state.firstName} onChange={this.handleChange}/>
+                                  <Input s={6} label="Last Name" id="lastName" value={this.state.lastName} onChange={this.handleChange}/>
+                                  <Input s={12} label="Age" id="age" value={this.state.age} onChange={this.handleChange}/>
+                                  <Input type="email" label="Email"s={12} id="email" value={this.state.email} onChange={this.handleChange}/>
+                                  <Input type="password" label="password" s={12} id="password" value={this.state.password} onChange={this.handleChange}/>
+
+                                  <Button waves='light'  className="mainBtn" type="submit" onClick={this.handleNewChild}>Submit</Button>
+                                </form>
+                              </Row>
+                            </Modal>
+                            
+                            <NavItem>Edit An Exisiting Child</NavItem>
+                          </Dropdown>
+
+                           {/* MENU FOOTER */}
+                          <Footer className="page-footer">
+                              <Button waves='light' className="mainBtn">LogOut</Button>
+                          </Footer>
+
+                        </SideNav>
+                      {/* END OF SIDE NAVBAR */}
                   
-                  <Button type="submit" waves='light' className="mainBtn">Submit</Button>
-                </form>
-              </Row>
-            </Modal>
-            
-            <NavItem>Edit An Existing Task</NavItem>
-          </Dropdown>
-          
-           {/* MANAGE CHILDREN */}
-          <Dropdown trigger={
-              <SideNavItem href='#!second' icon='face'>Manage Children</SideNavItem>
-            }>
+                  </li>
 
-          {/* MODAL FOR NEW CHILD */}
-            <Modal
-              header='Set Up a New Account for Child'
-              fixedFooter
-              trigger={
-                <NavItem>Add A New Child</NavItem>
-              }>
-              <Row>
-                <form>
-                  <Input s={6} label="First Name" id="firstName" value={this.state.firstName} onChange={this.handleChange}/>
-                  <Input s={6} label="Last Name" id="lastName" value={this.state.lastName} onChange={this.handleChange}/>
-                  <Input s={12} label="Age" id="age" value={this.state.age} onChange={this.handleChange}/>
-                  <Input type="email" label="Email"s={12} id="email" value={this.state.email} onChange={this.handleChange}/>
-                  <Input type="password" label="password" s={12} id="password" value={this.state.password} onChange={this.handleChange}/>
+          </Navbar>
 
-                  <Button waves='light'  className="mainBtn" type="submit" onClick={this.handleNewChild}>Submit</Button>
-                </form>
-              </Row>
-            </Modal>
-            
-            <NavItem>Edit An Exisiting Child</NavItem>
-          </Dropdown>
+        </div>  
 
-           {/* MENU FOOTER */}
-          <Footer className="page-footer">
-              <Button waves='light' className="mainBtn">LogOut</Button>
-          </Footer>
-
-        </SideNav>
-      {/* END OF SIDE NAVBAR */}
-
+      </header>
 
       {/* BEGIN PAGE CONTENT */}
 
